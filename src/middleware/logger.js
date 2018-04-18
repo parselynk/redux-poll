b69424e3a8a765921383b16e49cce7387037b759
Reza Kay <witch5062@gmail.com>
2018-04-18 18:05:49 +0800

    Logger middle ware modification

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,10 +1,13 @@
 const logger = (store) => (next) => (action) => {
     console.group(action.type)
-    console.log('The action: ' + action)
+    console.log('The action: ')
+    console.log(action)
     const returnValue = next(action)
-    console.log('The new state: ' + store.getState())
-    return returnValue
+    console.log('The new state: ')
+    console.log(store.getState())
     console.groupEnd()
+
+    return returnValue
 }
 
 export default logger
