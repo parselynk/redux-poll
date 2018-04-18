ec3a5c3b04a1afd837f97f51110e7b178f2d61d5
Reza Kay <witch5062@gmail.com>
2018-04-18 16:49:05 +0800

    index
    - reducers added
    - Provider added
    - middleware added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+import middleware from './middleware'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers, middleware)
+console.log(store.getState())
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>, 
+document.getElementById('root'))
+
